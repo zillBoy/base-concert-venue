536f59737e60968bb4d4302ebeed7a6be99e9c59
Muhammad Usman <musman0741@gmail.com>
2023-07-15 18:22:34 +0500

    [master] test added for brand.test.tsx

---
+++ __tests__/ui/band.test.tsx
@@ -0,0 +1,17 @@
+// Extenral Dependencies
+import { render, screen } from "@testing-library/react";
+
+import { readFakeData } from "@/__tests__/__mocks__/fakeData/index";
+import BandComponent from "@/pages/bands/[bandId]";
+
+describe("Band", () => {
+  test("band component displays correct band information", async () => {
+    const { fakeBands } = await readFakeData();
+    render(<BandComponent band={fakeBands[0]} error={null} />);
+
+    const heading = screen.getByRole("heading", {
+      name: /the wandering bunnies/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
