0eb0cc56fbf484dbf2dd2f40feca974254de7236
Muhammad Usman <musman0741@gmail.com>
2023-07-15 19:02:17 +0500

    [master] msw added, eslint updated to include "jest"

---
+++ __tests__/__mocks__/msw/handlers.ts
@@ -0,0 +1,10 @@
+import { rest } from "msw";
+
+import { readFakeData } from "../fakeData";
+
+export const handlers = [
+  rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    return res(ctx.json({ show: fakeShows[0] }));
+  }),
+];
