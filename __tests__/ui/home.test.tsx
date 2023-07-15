f42b6f6aa0c5743c755ea1625ec53aad3668f9ce
Muhammad Usman <musman0741@gmail.com>
2023-07-15 18:07:47 +0500

    [master] test and code added for index.js, also updated the setthings.json file

---
+++ __tests__/ui/home.test.tsx
@@ -0,0 +1,20 @@
+// Extenral Dependenices
+import { render, screen } from "@testing-library/react";
+
+import Home from "@/pages/index";
+
+describe("Home", () => {
+  test("page has correct heading and image", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: "Welcome to Popular Concert Venue",
+    });
+    expect(heading).toBeInTheDocument();
+
+    const image = screen.getByRole("img", {
+      name: "Concert goer with hands in the shape of a heart",
+    });
+    expect(image).toBeInTheDocument();
+  });
+});
