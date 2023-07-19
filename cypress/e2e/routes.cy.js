00507aa1c59866323b5ce4a81c98f9f66372ffe9
Muhammad Usman <musman0741@gmail.com>
2023-07-19 18:13:26 +0500

    [feature/dynamic-route-build-time-page-test] added test and new task named addBand in order to add band that is not found during build time

--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
+import { generateNewBand } from '../../__tests__/__mocks__/fakeData/newBand'
+import { generateRandomId } from '../../lib/features/reservations/utils';
+
 it("displays correct heading when navigating to shows route", () => {
   cy.visit("/");
   cy.findByRole("button", { name: /shows/i }).click();
@@ -15,6 +18,20 @@ it("displays correct heading when navigating to bands route", () => {
   }).should("exist");
 });
 
-it("reset the database", () => {
-  cy.task("db:reset");
+it("displays correct band name for band route that existed at build time", () => {
+  cy.task("db:reset").visit("/bands/1");
+  cy.findByRole("heading", { name: /shamrock pete/i }).should("exist");
+});
+
+it("dispalys error message when the band does not exist", () => {
+  cy.task("db:reset").visit("/bands/12345");
+  cy.findByRole("heading", { name: /error: band not found/i }).should("exist");
+});
+
+it("display name for band that was not present at build time", () => {
+  const bandId = generateRandomId();
+  const newBand = generateNewBand(bandId);
+
+  cy.task("db:reset").task("addBand", newBand).visit(`/bands/${bandId}`);
+  cy.findByRole("heading", { name: /avalanche of cheese/i }).should("exist");
 });
