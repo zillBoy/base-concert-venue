5fc7a48c4daa9e81ade789a218e0421311677aa2
Muhammad Usman <41048737+zillBoy@users.noreply.github.com>
2023-07-19 18:14:23 +0500

    [feature/dynamic-route-build-time-page-test] added test and new task named addBand in order to add band that is not found during build time (#3)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "cypress";
 
 import { resetDB } from "./__tests__/__mocks__/db/utils/reset-db";
+import { addBand } from "./lib/features/bands/queries";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task", {
         "db:reset": () => resetDB().then(() => null),
+        addBand: (newBand) => addBand(newBand).then(() => null),
       });
     },
   },
