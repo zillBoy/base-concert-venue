80be823e9336bab786887866542764c846c74978
Muhammad Usman <41048737+zillBoy@users.noreply.github.com>
2023-07-19 17:56:06 +0500

    [feature/static-route-bands] test added (#2)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
 
+import { resetDB } from "./__tests__/__mocks__/db/utils/reset-db";
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        "db:reset": () => resetDB().then(() => null),
+      });
     },
   },
 });
--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -6,3 +6,15 @@ it("displays correct heading when navigating to shows route", () => {
   cy.findByRole("button", { name: /shows/i }).click();
   cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
 });
+
+it("displays correct heading when navigating to bands route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", {
+    name: /our illustrious performers/i,
+  }).should("exist");
+});
+
+it("reset the database", () => {
+  cy.task("db:reset");
+});
